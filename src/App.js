37db1c7d3fefb4add9e2140d954bc1d16d6bac3b
dwaynediddy <burrows.dwayne@icloud.com>
2021-01-05 11:24:50 +1000

    added google map api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
+import GMap from './Gmap'
 
-function App() {
-  return (
-    <h1>Best Spot</h1>
-  )
+const GOOGLE_MAP_API_KEY = 'AIzaSyDqOTmEdqJJ_EqvZNknrWjVdC5wrIr9JPQ'
+
+const loadGoogleMapScript = (callback) => {
+  if (typeof window.google === 'object' && typeof window.google.maps === 'object') {
+    callback();
+  } else {
+    const googleMapScript = document.createElement("script");
+    googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}`;
+    window.document.body.appendChild(googleMapScript);
+    googleMapScript.addEventListener("load", callback);
+  }
 }
-
-export default App;
+ 
+const App = () => {
+  const [loadMap, setLoadMap] = useState(false);
+ 
+  useEffect(() => {
+    loadGoogleMapScript(() => {
+      setLoadMap(true)
+    });
+  }, []);
+ 
+  return (
+    <div className="App">
+      {!loadMap ? <div>Loading...</div> : <GMap />}
+    </div>
+  );
+}
+ 
+export default App
